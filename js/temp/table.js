4a22038e56d8b8e94c8a0d3e9ac45f4301acfdd5
dani ferdinan <daniferdinandall@gmail.com>
2023-06-25 13:04:50 +0700

    git push

--- js/temp/table.js
+++ js/temp/table.js
@@ -13,30 +13,6 @@ export let isiTabel =
             <p class="text-xs font-medium text-coolGray-500">#FAKULTAS#</p>
         </div>
     </td>
-    <td class="whitespace-nowrap px-6 py-4">
-        <div class="w-auto">
-            <p class="text-s font-semibold text-coolGray-800">#MATKUL#</p>
-            <p class="text-s font-medium text-coolGray-800">Grade: #NILAI#</p>
-            </div>
-        </td>
-    <td class="whitespace-nowrap px-6 py-4">
-        <div class="w-auto">
-            <p class="text-s font-semibold text-coolGray-800">#MATKUL1#</p>
-            <p class="text-s font-medium text-coolGray-800">Grade: #NILAI1#</p>
-        </div>
-    </td>
-    <td class="whitespace-nowrap px-6 py-4">
-        <div class="w-auto">
-            <p class="text-s font-semibold text-coolGray-800">#MATKUL2#</p>
-            <p class="text-s font-medium text-coolGray-800">Grade: #NILAI2#</p>
-        </div>
-    </td>
-    <td class="whitespace-nowrap px-6 py-4">
-        <div class="w-auto">
-            <p class="text-s font-semibold text-coolGray-800">#MATKUL3#</p>
-            <p class="text-s font-medium text-coolGray-800">Grade: #NILAI3#</p>
-        </div>
-    </td>
 </tr>
 `
 export let isiTabelDosen =
