6d6cc8785ff3018be0e43a9dbfc9cf86b1a46600
dani ferdinan <daniferdinandall@gmail.com>
2023-06-30 23:50:21 +0700

    update url

--- js/config/url.js
+++ js/config/url.js
@@ -1,4 +1,15 @@
-export let urlAPIdhs = "https://ws-dani.herokuapp.com/dhs-all";
-export let urlAPImatkul = "https://ws-dani.herokuapp.com/matkul-all";
-export let urlAPIdosen = "https://ws-dani.herokuapp.com/dosen-all";
-export let urlAPImahasiswa = "https://ws-dani.herokuapp.com/mahasiswa-all";
+export const urlAPIdhs = "https://ws-dani-pemrog3-b1fcf6b5d6ca.herokuapp.com/dhs";
+
+export const urlAPImatkul = "https://ws-dani-pemrog3-b1fcf6b5d6ca.herokuapp.com/matakuliah";
+
+export const urlAPIdosen = "https://ws-dani-pemrog3-b1fcf6b5d6ca.herokuapp.com/dosen";
+
+export const urlAPImahasiswa = "https://ws-dani-pemrog3-b1fcf6b5d6ca.herokuapp.com/mahasiswa";
+
+// export const urlAPIdhs = "http://127.0.0.1:8000/dhs";
+
+// export const urlAPImatkul = "http://127.0.0.1:8000/matakuliah";
+
+// export const urlAPIdosen = "http://127.0.0.1:8000/dosen";
+
+// export const urlAPImahasiswa = "http://127.0.0.1:8000/mahasiswa";
