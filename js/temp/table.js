5ea48df65c53aa44f915dbcefe46078c9f7939f6
dani ferdinan <daniferdinandall@gmail.com>
2023-07-06 06:44:44 +0700

    UPDATE

--- js/temp/table.js
+++ js/temp/table.js
@@ -43,8 +43,12 @@ export let isiTabelDosen =
 export let isiTabelMatkul =
     `
     <tr class="border-b dark:border-neutral-500">
-    <td class="whitespace-nowrap px-6 py-4 font-medium">#KODE#</td>
-    <td class="whitespace-nowrap px-6 py-4">#NAMA#</td>
+    <td class="whitespace-nowrap px-6 py-4 font-medium">
+        <div class="w-auto">
+            <p class="text-s font-semibold text-coolGray-800">#NAMA#</p>
+            <p class="text-xs font-medium text-coolGray-500">#KODE#</p>
+        </div>
+    </td>
     <td class="whitespace-nowrap px-6 py-4">#DOSEN#</td>
     <td class="whitespace-nowrap px-6 py-4">#SKS#</td>
     <td class="whitespace-nowrap px-6 py-4">
